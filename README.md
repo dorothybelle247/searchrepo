fc4cb8591daeca5fe32da930585386596f43f7d1
dorothybelle247 <dorothybelle247@gmail.com>
2021-04-16 14:39:22 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-q652hh
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-q652hh)
---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./style.css";
+import { Dashboard, Login, PrivateRoute, AuthWrapper, Error } from './pages';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+export default function App() {
+  return (
+    <div>
+        <Dashboard></Dashboard>
+      <Login />
+      <Error />
+    </div>
+  );
+}
